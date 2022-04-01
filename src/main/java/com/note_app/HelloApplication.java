1e129875a38b537677a7bc1339a7bb6294bbdc9e
ridoineel <ridoineelofficiel@gmail.com>
2022-04-01 07:09:47 +0000

    add UI components

--- src/main/java/com/note_app/HelloApplication.java
+++ src/main/java/com/note_app/HelloApplication.java
@@ -1,8 +1,11 @@
 package com.note_app;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +15,27 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("render.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
         stage.setTitle("Note App");
         stage.setScene(scene);
+        stage.setMinWidth(300);
+        stage.setMinHeight(300);
         stage.show();
+
+        stage.setOnCloseRequest((event) -> {
+            event.consume();
+            logout(stage);
+        });
+    }
+    public void logout(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are about logout");
+        alert.setContentText("Do you want to logout ?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage.close();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/note_app/HelloController.java
+++ src/main/java/com/note_app/HelloController.java
@@ -1,14 +1,67 @@
 package com.note_app;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class HelloController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private AnchorPane scenePane;
+    @FXML
+    private TextArea textArea;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("ridoine choose");
+
+        //scenePane.getChildren().add((Node) fileChooser);
+    }
+
+    public void newFile(ActionEvent event) {
+
+    }
+
+    public void openFile(ActionEvent event) {
+
+    }
+
+    public void saveFile(ActionEvent event) {
+
+    }
+
+    public void updateTextArea(String text) {
+        textArea.setText(text);
+    }
+
+    public void writeInFile(String file_path, String content) throws IOException {
+        FileWriter file = new FileWriter(file_path);
+
+        file.write(content);
+    }
+
+    public void logout(ActionEvent e) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are about logout");
+        alert.setContentText("Do you want to logout ?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            Stage stage = (Stage) scenePane.getScene().getWindow();
+            stage.close();
+        }
     }
 }
